9bf09ceadbdf5165be2b5939c7a4d13f104af274
Aso2001409 <2001409@s.asojuku.ac.jp>
2021-12-03 12:45:35 +0900

    新規登録更新

--- PHP/newuser-ok.php
+++ PHP/newuser-ok.php
@@ -9,22 +9,20 @@
         <div class="box">
             <div class="box-user">
                 <?php
-                $sql=$pdo->prepare('insert into customers values (null,?,?,?,?,?,CURDATE(),0)');
-                $sql->execute([$_POST['pass'],$_POST['name'],$_POST['mail'],$_POST['postal'],$_POST['address']]);
+                $customer_id=1;
+                foreach ($pdo->query('select max(customer_id) from customers') as $row) {
+                    $purchase_id = (int)$row['max(customer_id)'] + 1;
+                }
+                $sql=$pdo->prepare('insert into customers values (?,?,?,?,?,?,CURDATE(),0)');
+                $sql->execute([$purchase_id,$_POST['pass'],$_POST['name'],$_POST['mail'],$_POST['postal'],$_POST['address']]);
                 echo '会員登録しました。';
 
+                $_SESSION['customer']['id'] = $purchase_id;
                 $_SESSION['customer']['name']=$_POST['name'];
                 $_SESSION['customer']['address']=$_POST['address'];
                 $_SESSION['customer']['postal']=$_POST['postal'];
                 $_SESSION['customer']['mail']=$_POST['mail'];
                 $_SESSION['customer']['pass']=$_POST['pass'];
-
-
-                $sql=$pdo->prepare('select * from customers where mail=? and password=?');
-                $sql->execute([$_POST['mail'],$_POST['pass']]);
-                foreach ($sql as $row) {
-                    $_SESSION['customer']['id'] = $row['customer_id'];
-                }
 
                 ?>
             </div>
