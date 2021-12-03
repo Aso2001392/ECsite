f03250fcc21f3c6b996b49a7f4b2c82cebe4e888
Aso2001409 <2001409@s.asojuku.ac.jp>
2021-12-03 15:21:30 +0900

    新規登録更新ID更新12/3

--- PHP/newuser-ok.php
+++ PHP/newuser-ok.php
@@ -9,15 +9,15 @@
         <div class="box">
             <div class="box-user">
                 <?php
-                $customer_id=1;
+                $id=1;
                 foreach ($pdo->query('select max(customer_id) from customers') as $row) {
-                    $customer_id = (int)$row['max(customer_id)'] + 1;
+                    $id = (int)$row['max(customer_id)'] + 1;
                 }
                 $sql=$pdo->prepare('insert into customers values (?,?,?,?,?,?,CURDATE(),0)');
-                $sql->execute([$customer_id,$_POST['pass'],$_POST['name'],$_POST['mail'],$_POST['postal'],$_POST['address']]);
+                $sql->execute([$id,$_POST['pass'],$_POST['name'],$_POST['mail'],$_POST['postal'],$_POST['address']]);
                 echo '会員登録しました。';
 
-                $_SESSION['customer']['id'] = $customer_id;
+                $_SESSION['customer']['id'] = $id;
                 $_SESSION['customer']['name']=$_POST['name'];
                 $_SESSION['customer']['address']=$_POST['address'];
                 $_SESSION['customer']['postal']=$_POST['postal'];
