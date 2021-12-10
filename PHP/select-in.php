57b25c722976cbf72cd5e1074703d1e517202ad2
野出 <2001392@s.asojuku.ac.jp>
2021-12-10 16:14:12 +0900

    アンケート画面修正　12/10

--- PHP/select-in.php
+++ PHP/select-in.php
@@ -9,62 +9,62 @@
         <div class="box">
             <div class="ncen">
                 <h1 class="contact-title">香水アンケート回答入力</h1>
-                <p>アンケートに答えて、「回答送信」ボタンをクリックしてください。</p>
+                <p>アンケートに答えて、「回答送信」ボタンをクリックしてください。</p><br>
                 <form action="select-out.php" method="post" name="form" onsubmit="return validate()">
                     <label >Q1.花の匂いが好きだ</label>
                     <div class="select">
-                        <input type="radio" class="abc" name="flower" value="2" required>はい
-                        <input type="radio" class="abc" name="flower" value="0" required>いいえ
-                        <input type="radio" class="abc" name="flower" value="1" required>どちらでもない<br>
+                        <label class="checkbox"><input type="radio" class="abc" name="flower" value="2" required>はい</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="flower" value="0" required>いいえ</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="flower" value="1" required>どちらでもない</label><br>
                     </div>
                     <hr class="sen"></hr>
 
 
                     <label>Q2.果物の匂いが好き</label>
                     <div class="select">
-                        <input type="radio" class="abc" name="fruit" value="2" required>はい
-                        <input type="radio" class="abc" name="fruit" value="0" required>いいえ
-                        <input type="radio" class="abc" name="fruit" value="1" required>どちらでもない<br>
+                        <label class="checkbox"><input type="radio" class="abc" name="fruit" value="2" required>はい</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="fruit" value="0" required>いいえ</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="fruit" value="1" required>どちらでもない</label><br>
                     </div>
                     <hr class="sen"></hr>
 
                     <label>甘い匂いが好き</label>
                     <div class="select">
-                        <input type="radio" class="abc" name="sweet" value="2" required>はい
-                        <input type="radio" class="abc" name="sweet" value="0" required>いいえ
-                        <input type="radio" class="abc" name="sweet" value="1" required>どちらでもない<br>
+                        <label class="checkbox"><input type="radio" class="abc" name="sweet" value="2" required>はい</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="sweet" value="0" required>いいえ</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="sweet" value="1" required>どちらでもない</label><br>
                     </div>
                     <hr class="sen"></hr>
 
                     <label>石鹸の匂いが好き</label>
                     <div class="select">
-                        <input type="radio" class="abc" name="soap" value="2" required>はい
-                        <input type="radio" class="abc" name="soap" value="0" required>いいえ
-                        <input type="radio" class="abc" name="soap" value="1" required>どちらでもない<br>
+                        <label class="checkbox"><input type="radio" class="abc" name="soap" value="2" required>はい</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="soap" value="0" required>いいえ</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="soap" value="1" required>どちらでもない</label><br>
                     </div>
                     <hr class="sen"></hr>
 
                     <label>海の匂いが好き</label>
                     <div class="select">
-                        <input type="radio" class="abc" name="sea" value="2" required>はい
-                        <input type="radio" class="abc" name="sea" value="0" required>いいえ
-                        <input type="radio" class="abc" name="sea" value="1" required>どちらでもない<br>
+                        <label class="checkbox"><input type="radio" class="abc" name="sea" value="2" required>はい</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="sea" value="0" required>いいえ</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="sea" value="1" required>どちらでもない</label><br>
                     </div>
                     <hr class="sen"></hr>
 
                     <label>木や森の匂いが好き</label>
                     <div class="select">
-                        <input type="radio" class="abc" name="tree" value="2" required>はい
-                        <input type="radio" class="abc" name="tree" value="0" required>いいえ
-                        <input type="radio" class="abc" name="tree" value="1" required>どちらでもない<br>
+                        <label class="checkbox"><input type="radio" class="abc" name="tree" value="2" required>はい</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="tree" value="0" required>いいえ</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="tree" value="1" required>どちらでもない</label><br>
                     </div>
                     <hr class="sen"></hr>
 
                     <label>季節で夏が好き</label>
                     <div class="select">
-                        <input type="radio" class="abc" name="summer" value="2" required>はい
-                        <input type="radio" class="abc" name="summer" value="0" required>いいえ
-                        <input type="radio" class="abc" name="summer" value="1" required>どちらでもない<br>
+                        <label class="checkbox"><input type="radio" class="abc" name="summer" value="2" required>はい</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="summer" value="0" required>いいえ</label>
+                        <label class="checkbox"><input type="radio" class="abc" name="summer" value="1" required>どちらでもない</label><br>
                     </div>
                     <hr class="sen"></hr>
 
