195cb5f64f5f1c2ef983b9de447dbc6a9904c4a0
Aso2001409 <2001409@s.asojuku.ac.jp>
2021-12-15 11:41:33 +0900

    戻る押しても入力消えなくした　12/15

--- PHP/newuser-in.php
+++ PHP/newuser-in.php
@@ -1,41 +1,59 @@
 <?php require_once 'css/header.php'; ?>
-<div class="main">
-    <!-- サイドバー -->
-    <?php require_once 'css/menu.php'; ?>
+<?php
+if(isset($_POST['name'],$_POST['postal'],$_POST['address'])){
+    $name=$_POST['name'];
+    $postal=$_POST['postal'];
+    $address=$_POST['address'];
+    if(isset($_POST['mail'])){
+        $mail=$_POST['mail'];
+    }else{
+        $mail="";
+    }
+}else{
+    $name="";
+    $postal="";
+    $address="";
+    $mail="";
+}
+?>
+    <div class="main">
+        <!-- サイドバー -->
+        <?php require_once 'css/menu.php'; ?>
 
-    <!-- メイン画面 -->
-    <div class="contentB">
+        <!-- メイン画面 -->
+        <div class="contentB">
 
-        <div class="box">
-            <h3>新規登録</h3>
-            <div class="box-user">
-                <form action="newuser-out.php" method="post">
-                    <div id="usertitle">名前<br>
-                        <input class="box1" type="text" name="name"  placeholder="Name" required></div>
+            <div class="box">
+                <h3>新規登録</h3>
+                <div class="box-user">
+                    <form action="newuser-out.php" method="post">
+                        <div id="usertitle">名前<br>
+                            <input class="box1" type="text" name="name"  placeholder="Name" value="<?=$name?>" required></div>
 
-                    <div id="usertitle">郵便番号<br>
-                        <p style="color:crimson">※ハイフンなしの半角数字7桁で入力してください</p>
-                        <input class="box1" type="text" name="postal" id="郵便番号" onKeyUp="$('#郵便番号').zip2addr('#住所');" placeholder="Postal code" pattern="^([0-9]{7})$" title="※ハイフンなしの半角数字7桁で入力してください" autocomplete="off" required></div>
+                        <div id="usertitle">郵便番号<br>
+                            <p style="color:crimson">※ハイフンなしの半角数字7桁で入力してください</p>
+                            <input class="box1" type="text" name="postal" id="郵便番号" maxlength=7 value="<?=$postal?>" onKeyUp="$('#郵便番号').zip2addr('#住所');" placeholder="Postal code" pattern="^([0-9]{7})$" title="※ハイフンなしの半角数字7桁で入力してください" autocomplete="off" required></div>
 
-                    <div id="usertitle">住所<br>
-                        <input class="box1" type="text" name="address" id="住所" placeholder="Address" required></div>
+                        <div id="usertitle">住所<br>
+                            <input class="box1" type="text" name="address" id="住所" placeholder="Address" value="<?=$address?>" required></div>
 
-                    <div id="usertitle">メールアドレス<br>
-                        <input class="box1" type="email" name="mail" placeholder="Email" required></div>
+                        <div id="usertitle">メールアドレス<br>
+                            <input class="box1" type="email" name="mail" placeholder="Email" value="<?=$mail?>" required></div>
 
-                    <div id="usertitle">メールアドレス(確認用)<br>
-                        <p style="color:crimson">※上記と同じメールアドレスを入力してください。</p>
-                        <input class="box1" type="email" name="mail2" placeholder="Email" required></div>
+                        <div id="usertitle">メールアドレス(確認用)<br>
+                            <p style="color:crimson">※上記と同じメールアドレスを入力してください。</p>
+                            <input class="box1" type="email" name="mail2" placeholder="Email" value="<?=$mail?>" required></div>
 
-                    <div id="usertitle">パスワード<br>
-                        <p style="color:crimson">※半角英数字4文字以上16文字以下で入力してください。</p>
-                        <input class="box1" type="password" name="pass" placeholder="Password" pattern="^([a-zA-Z0-9]{4,16})$" title="※半角英数字4文字以上16文字以下で入力してください。" required></div>
+                        <div id="usertitle">パスワード<br>
+                            <p style="color:crimson">※半角英数字4文字以上16文字以下で入力してください。</p>
+                            <input class="box1" type="password" name="pass" placeholder="Password" pattern="^([a-zA-Z0-9]{4,16})$" title="※半角英数字4文字以上16文字以下で入力してください。" required></div>
 
-                    <br><br>
+                        <br><br>
+                </div>
+                <a href="login.php" class="button">戻る</a>　　<button type="submit" value="send" class="button">登録</button>
+                </form>
             </div>
-            <a href="login.php" class="button">戻る</a>　　<button type="submit" value="send" class="button">登録</button>
-            </form>
         </div>
     </div>
-</div>
+
 <?php require 'css/footer.php'; ?>
--- PHP/newuser-out.php
+++ PHP/newuser-out.php
@@ -10,27 +10,33 @@
                 <!--$_POST['mail']と$_POST['mail2']を比較-->
                 <?php if($_POST['mail'] === $_POST['mail2']) { ?>
 
-                <h3>下記内容でお間違いがなければ確定を押してください。</h3>
+                    <h3>下記内容でお間違いがなければ確定を押してください。</h3>
 
-                <div class="box-user">
-                    <div id="usertitle">氏名 : <?=$_POST['name'] ?> </div>
-                    <div id="usertitle">郵便番号 : <?=$_POST['postal'] ?> </div>
-                    <div id="usertitle">住所 : <?=$_POST['address'] ?> </div>
-                    <div id="usertitle">メールアドレス : <?=$_POST['mail'] ?> </div>
-                    <div id="usertitle">パスワード : <?=$_POST['pass'] ?> </div>
+                    <div class="box-user">
+                        <div id="usertitle">氏名 : <?=$_POST['name'] ?> </div>
+                        <div id="usertitle">郵便番号 : <?=$_POST['postal'] ?> </div>
+                        <div id="usertitle">住所 : <?=$_POST['address'] ?> </div>
+                        <div id="usertitle">メールアドレス : <?=$_POST['mail'] ?> </div>
+                        <div id="usertitle">パスワード : <?=$_POST['pass'] ?> </div>
 
-                    <form action="newuser-ok.php" method="post">
-                        <input type="hidden" name="name" value="<?=$_POST['name'] ?>" />
-                        <input type="hidden" name="postal" value="<?=$_POST['postal'] ?>" />
-                        <input type="hidden" name="address" value="<?=$_POST['address'] ?>" />
-                        <input type="hidden" name="mail" value="<?=$_POST['mail'] ?>" />
-                        <input type="hidden" name="pass" value="<?=$_POST['pass'] ?>" />
-                        <p><a href="newuser-in.php" class="button">戻る</a><button type="submit" value="send" class="button" >確定</button></p></div>
+                        <form>
+                            <input type="hidden" name="name" value="<?=$_POST['name'] ?>" />
+                            <input type="hidden" name="postal" value="<?=$_POST['postal'] ?>" />
+                            <input type="hidden" name="address" value="<?=$_POST['address'] ?>" />
+                            <input type="hidden" name="mail" value="<?=$_POST['mail'] ?>" />
+                            <input type="hidden" name="pass" value="<?=$_POST['pass'] ?>" />
+                            <p><button type="submit" value="send" class="button" formaction="newuser-in.php" formmethod="post">戻る</button>
+                                <button type="submit" value="send" class="button" formaction="newuser-ok.php" formmethod="post">確定</button></p></div>
                     </form>
 
                 <?php } else { ?>
                     <h3>メールアドレスが違います。</h3>
-                    <a href="newuser-in.php" class="button" />戻る</a>
+                    <form>
+                        <input type="hidden" name="name" value="<?=$_POST['name'] ?>" />
+                        <input type="hidden" name="postal" value="<?=$_POST['postal'] ?>" />
+                        <input type="hidden" name="address" value="<?=$_POST['address'] ?>" />
+                        <button type="submit" value="send" class="button" formaction="newuser-in.php" formmethod="post">戻る</button>
+                    </form>
                 <?php } ?>
             </div>
         </div>
