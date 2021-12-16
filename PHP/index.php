aa71c6410b112d5303cecd51b8348b603b5ae369
Aso2001409 <2001409@s.asojuku.ac.jp>
2021-12-16 12:47:25 +0900

    pick up ランダム化　12/16

--- PHP/index.php
+++ PHP/index.php
@@ -25,18 +25,45 @@
                     ?>
                 </div>
                 <p class="pick-text">PICK UP</p>
+                <?php
+                $id= $item1=$item2=$item3=0;
+                $sql=$pdo->query('select max(item_id) from items');
+                foreach ($sql as $row){
+                    $id=$row['max(item_id)'];
+                }
+                $id = preg_replace('/[^0-9]/', '', $id);
+                //値の配列を生成する
+                $array = range(1, $id);
+                //配列をシャッフルする
+                shuffle($array);
+                //配列の上から3番目まで切り取る
+                $array = array_slice($array, 0, 3);
+
+                $item1=$array[0];
+                if($item1<=999){
+                    $item1=sprintf('%04d',$item1);
+                }
+                $item2=$array[1];
+                if($item2<=999){
+                    $item2=sprintf('%04d',$item2);
+                }
+                $item3=$array[2];
+                if($item3<=999){
+                    $item3=sprintf('%04d',$item3);
+                }
+                ?>
                 <div class="img_pick">
                     <div class="tate">
-                        <a href="detail.php?id=I0022"><img src="img/items/I0022.jpg" class="check1"></a><br>
-                        <a class="check2" href="detail.php?id=I0022">check more</a>
+                        <a href="detail.php?id=I<?=$item1?>"><img src="img/items/I<?=$item1?>.jpg" class="check1"></a><br>
+                        <a class="check2" href="detail.php?id=I<?=$item1?>">check more</a>
                     </div>
                     <div class="tate">
-                        <a href="detail.php?id=I0042"><img src="img/items/I0042.jpg" class="check1"></a><br>
-                        <a class="check2" href="detail.php?id=I0042">check more</a>
+                        <a href="detail.php?id=I<?=$item2?>"><img src="img/items/I<?=$item2?>.jpg" class="check1"></a><br>
+                        <a class="check2" href="detail.php?id=I<?=$item2?>">check more</a>
                     </div>
                     <div class="tate">
-                        <a href="detail.php?id=I0035"><img src="img/items/I0035.jpg" class="check1"></a><br>
-                        <a class="check2" href="detail.php?id=I0035">check more</a>
+                        <a href="detail.php?id=I<?=$item3?>"><img src="img/items/I<?=$item3?>.jpg" class="check1"></a><br>
+                        <a class="check2" href="detail.php?id=I<?=$item3?>">check more</a>
                     </div>
                 </div>
                 <img src="img/baner.jpg" class="check3">
